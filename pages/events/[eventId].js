e092b15c63246aefa1b9360e36f70526fe42ade8
eptakam <emmataks@gmail.com>
2024-07-15 15:15:10 -0300

    Setting up the main pages.

---
+++ pages/events/[eventId].js
@@ -0,0 +1,7 @@
+export default function EventsDetailPage() {
+  return (
+    <div>
+      <h1>The Events Detail Page</h1>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Homepage(){
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+}
