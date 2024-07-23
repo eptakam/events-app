7f017d0f4653b1c2ee67fdce4a5ef1553281d8b1
eptakam <emmataks@gmail.com>
2024-07-23 09:20:11 -0300

    Configuring the head content.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 /*
-    si nous voulons afficher les donnees a partir d'une API externe, une questio se pose: comment obtenir ces donnees? allons-nous utliser client-side fetching ou server-side rendering?
+    si nous voulons afficher les donnees a partir d'une API externe, une question se pose: comment obtenir ces donnees? allons-nous utliser client-side fetching ou server-side rendering?
 
     vue que ce n'est pas une page qui sera mis a jour frequemment, ce serait une bonne idee d'utiliser le server-side rendering 'getStaticProps' pour obtenir les donnees et les afficher sur la page d'accueil.
 */
+
+// optimisation de la page d'acceuil
+import Head from "next/head";
 
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../helpers/api-util";
@@ -11,6 +14,14 @@ export default function Homepage(props) {
 
   return (
     <div>
+      {/* ceci est pour l'optimisation de la page d'acceuil: configuration de l'entete de la page */}
+      <Head> 
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
