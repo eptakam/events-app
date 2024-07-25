16dfca3594e9805fa90ef4d89378a9474e793b4c
eptakam <emmataks@gmail.com>
2024-07-25 15:36:50 -0300

    Adding Comments and NewsLetterRegistration components.

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -7,6 +7,7 @@ import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
+import Comments from "../../components/input/comments";
 
 // optimisation de la page
 import Head from "next/head";
@@ -51,6 +52,7 @@ export default function EventsDetailPage(props) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </Fragment>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import Head from "next/head";
 
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../helpers/api-util";
+import NewsletterRegistration from "../components/input/newsletter-registration";
 
 export default function Homepage(props) {
 
@@ -22,6 +23,7 @@ export default function Homepage(props) {
           content="Find a lot of great events that allow you to evolve..."
         />
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events} />
     </div>
   );
